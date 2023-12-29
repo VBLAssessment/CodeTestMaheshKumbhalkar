f69a077596294a5b6a1ae923f03d7adee46e9156
MaheshKumbhalkar <manishkumbhalkar@gmail.com>
2023-12-29 12:45:01 +0530

    Test Completed with the following points,
    1)Used OOP's pattern with test cases
    2)Extended the test cases. All test cases has been passed
    3) Implemented SendEmail() method.
    4) Added logs

--- CandidateCodeTest/CandidateCodeTest/CustomerServiceTests.cs
+++ CandidateCodeTest/CandidateCodeTest/CustomerServiceTests.cs
@@ -1,20 +1,119 @@
-﻿using Xunit;
+﻿using CandidateCodeTest.Common.Interfaces;
+using CandidateCodeTest.Services;
+using Moq;
+using System;
+using Xunit;
 
 namespace CandidateCodeTest
 {
     // Write the following unit tests
     public class CustomerServiceTests
     {
+        private CustomerService _customerService;
+        private Mock<IMessageService> _messageService;
+        private readonly Mock<ILogEntry> _AddLogsr;
+
+        public CustomerServiceTests()
+        {
+            //creating mock
+            _messageService = new Mock<IMessageService>();
+            _AddLogsr = new Mock<ILogEntry>();
+        }
+
+        /// <summary>
+        /// Test cases used to check the test in case of positive scenario
+        /// </summary>
         [Fact]
         public void Within_Time_Window_Email_Has_Been_Sent()
         {
-            
+            // Arrange
+            _messageService.Setup(m => m.SendEmail());
+            var startTime = new TimeSpan(0, 0, 0);
+            var endTime = new TimeSpan(23, 59, 59);
+            _customerService = new CustomerService(_messageService.Object, startTime, endTime, _AddLogsr.Object);
+
+            // Act
+            var result = _customerService.HasEmailBeenSent();
+
+            // Assert
+            Assert.True(result);
         }
 
+        /// <summary>
+        /// Test cases used to check the test in case of negative scenario
+        /// 
+        /// </summary>
         [Fact]
         public void Outside_Time_Window_Email_Has_Not_Been_Sent()
         {
-            
+            // Arrange
+            _messageService.Setup(m => m.SendEmail());
+            var startTime = new TimeSpan();
+            var endTime = new TimeSpan();
+            _customerService = new CustomerService(_messageService.Object, startTime, endTime, _AddLogsr.Object);
+
+            // Act
+            var result = _customerService.HasEmailBeenSent();
+
+            // Assert
+            Assert.False(result);
+        }
+
+        /// <summary>
+        /// Test cases used to check the test in case of negative scenario
+        /// </summary>
+        [Fact]
+        public void Case1_Email_Has_Not_Been_Sent()
+        {
+            // Arrange
+            _messageService.Setup(m => m.SendEmail());
+            var startTime = (TimeSpan?)null;
+            var endTime = new TimeSpan(23, 59, 59);
+            _customerService = new CustomerService(_messageService.Object, startTime, endTime, _AddLogsr.Object);
+
+            // Act
+            var result = _customerService.HasEmailBeenSent();
+
+            // Assert
+            Assert.False(result);
+        }
+
+        /// <summary>
+        /// Test cases used to check the test in case of negative scenario
+        /// </summary>
+        [Fact]
+        public void Case2_Email_Has_Not_Been_Sent()
+        {
+            // Arrange
+            _messageService.Setup(m => m.SendEmail());
+            var startTime = new TimeSpan(0, 0, 0);
+            var endTime = (TimeSpan?)null;
+            _customerService = new CustomerService(_messageService.Object, startTime, endTime, _AddLogsr.Object);
+
+            // Act
+            var result = _customerService.HasEmailBeenSent();
+
+            // Assert
+            Assert.False(result);
+        }
+
+        /// <summary>
+        /// Test cases used to check the test in case of negative scenario
+        /// </summary>
+        [Fact]
+        public void Case3_Email_Has_Not_Been_Sent()
+        {
+            // Arrange
+            _messageService.Setup(m => m.SendEmail());
+            var startTime = (TimeSpan?)null;
+            var endTime = (TimeSpan?)null;
+            _customerService = new CustomerService(_messageService.Object, startTime, endTime, _AddLogsr.Object);
+
+            // Act
+            var result = _customerService.HasEmailBeenSent();
+
+            // Assert
+            Assert.False(result);
         }
     }
 }
---
+++ CandidateCodeTest/CandidateCodeTest/Services/Impementation/CustomerService.cs
@@ -0,0 +1,53 @@
+﻿using CandidateCodeTest.Common.Interfaces;
+using CandidateCodeTest.Services;
+using System;
+
+namespace CandidateCodeTest
+{
+    public class CustomerService : ICustomerService
+    {
+        public ILogEntry _AddLogsr;
+        public IMessageService _messageService;
+        public TimeSpan? _startTime;
+        public TimeSpan? _endTime;
+        public CustomerService(IMessageService messageService, TimeSpan? startTime, TimeSpan? endTime, ILogEntry AddLogsr)
+        {
+            _AddLogsr = AddLogsr;
+            _startTime = startTime;
+            _endTime = endTime;
+            _messageService = messageService;
+        }
+        public bool HasEmailBeenSent()
+        {
+            _AddLogsr.AddLogs("HasEmailBeenSent Method Started");
+
+            try
+            {
+                TimeSpan now = DateTime.Now.TimeOfDay;
+                if (_startTime == null || _endTime == null)
+                {
+                    return false;
+                }
+
+                if ((now > _startTime) && (now < _endTime))
+                {
+                    _messageService.SendEmail();
+                    _AddLogsr.AddLogs("HasEmailBeenSent Method ended");
+                    return true;
+                }
+                else
+                {
+                    _AddLogsr.AddLogs("HasEmailBeenSent :  Email Sent Failed Due To Time Constraints");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _AddLogsr.AddLogs($"HasEmailBeenSent : Email Sent Failed, exception  : {ex.Message}");
+                return false;
+            }
+        }
+    }
+
+
+}
